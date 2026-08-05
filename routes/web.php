341e6cc7d282237f1be7d957907f3d6d8ae54c1e
fazanan <rudianto2008@gmail.com>
2026-08-05 11:27:51 +0700

    fix: allow all authenticated users to access settings/profile page

--- routes/web.php
+++ routes/web.php
@@ -461,9 +461,9 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
-    Volt::route('settings/profile', 'settings.profile')->middleware('menu:profil')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->middleware('menu:profil')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->middleware('menu:profil')->name('settings.appearance');
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
 require __DIR__.'/auth.php';
