2604ac5c0558a06c7335b69bed7e831b2e97a1d2
fazanan <rudianto2008@gmail.com>
2026-08-21 14:24:43 +0700

    Add seeder to create users based on bidang singkatan

---
+++ database/seeders/UserBidangBaruSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BidangDpd;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserBidangBaruSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $bidangs = BidangDpd::where('is_active', true)->whereNotNull('singkatan')->get();
+        $password = Hash::make('PKS123!');
+
+        foreach ($bidangs as $bidang) {
+            $singkatan = strtolower(trim($bidang->singkatan));
+            if (empty($singkatan)) {
+                continue;
+            }
+
+            $email = $singkatan . '@bekasihebat.com';
+            
+            // Nama user, misal: Pengurus BPKK
+            $name = 'Pengurus ' . strtoupper($singkatan);
+
+            User::updateOrCreate(
+                ['email' => $email],
+                [
+                    'name' => $name,
+                    'role' => User::ROLE_BIDANG,
+                    'bidang_slug' => $bidang->slug,
+                    'org_level' => User::ORG_LEVEL_DPD,
+                    'status' => 'aktif',
+                    'password' => $password,
+                    'email_verified_at' => now(),
+                ]
+            );
+        }
+    }
+}
